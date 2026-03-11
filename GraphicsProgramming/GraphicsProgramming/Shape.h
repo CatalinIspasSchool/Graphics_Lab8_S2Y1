914337da0d9416437c768ba7614be577dfb0bb3f
CatalinIspasSchool <catalin.ispas.echo02@bism.ro>
2026-03-11 16:22:00 +0200

    Uploaded the Project

---
+++ GraphicsProgramming/GraphicsProgramming/Shape.h
@@ -0,0 +1,16 @@
+#ifndef _SHAPE_H
+#define _SHAPE_H
+
+#include "glut.h"
+#include <gl/gl.h>
+#include <gl/glu.h>
+
+class Shape
+{
+public:
+	void render1();
+	void render2();
+	void render3();
+
+};
+#endif 
